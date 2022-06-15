d9241c51af8c2598b04571749edced5dd1fad80f
Firat Ozdemir <fozdemir@gmail.com>
2022-06-15 09:12:05 +0200

    make index passing voluntary, bugfix with getitem

--- src/generators.py
+++ src/generators.py
@@ -28,7 +28,7 @@ class Generator_Group_Sampler:
                     
 class Generator_Paired_Input_Output:
     '''Samples (in_key, out_key) sample pairs from fname_h5 and applies transforms on in_key and transforms_target on out_key'''
-    def __init__(self, fname_h5, inds, in_key, out_key, transforms=None, transforms_target=None, **kwargs):
+    def __init__(self, fname_h5, in_key, out_key, inds=None, transforms=None, transforms_target=None, **kwargs):
         self.fname_h5 = fname_h5
         self.inds = inds
         self.in_key = in_key
@@ -49,11 +49,13 @@ class Generator_Paired_Input_Output:
                 if len != fh[k].shape[0]:
                     raise AssertionError('Length of datasets vary across keys. %d vs %d' % (len, fh[k].shape[0]))
         self.len = len
+        if self.inds == None:
+            self.inds = np.arange(len)
 
     def __len__(self):
         return self.len
 
-    def __get_item__(self, idx): 
+    def __getitem__(self, idx): 
         with h5py.File(self.fname_h5, 'r') as fh:
             x = fh[self.in_key][idx,...]
             if self.transforms is not None:
@@ -69,5 +71,5 @@ class Generator_Paired_Input_Output:
         if self.shuffle:
             self.prng.shuffle(inds)
         for i in inds:
-            s = self.__get_item__(idx=i)
+            s = self.__getitem__(idx=i)
             yield s
